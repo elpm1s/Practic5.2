b51eb26c75ea84da3a1a608f09fe2c131de51f79
Alexander <Alexander@PC>
2024-09-25 10:49:00 +0300

    Добавьте файлы проекта.

---
+++ Task2.c
@@ -0,0 +1,22 @@
+#include <locale.h>      
+
+#include <stdio.h> 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void task2()       //номер 2
+{
+	setlocale(LC_ALL, "RUS");
+
+	float x, a, b, y, k = -4;
+
+	printf("введите х:\n");
+	scanf("%f", &x);
+
+	a = log((-1) * k * x);
+	b = exp(2 * x) + (a * x);
+	y = x * (a * a * a) + (b * b);
+	printf("Ответ: y=%.1f\n", y);
+	printf(" \n");
+	printf("(a=%f   b=%f)\n", a, b);
+}
