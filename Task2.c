1a8721dd9acb60cce2aa527e3e5d2f7aa14a9631
Ольховский Александр <alexander.olkhovsky.21@gmail.com>
2024-09-25 10:54:52 +0300

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -4,19 +4,19 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-void task2()       //номер 2
+void main()       //Г­Г®Г¬ГҐГ° 2
 {
 	setlocale(LC_ALL, "RUS");
 
 	float x, a, b, y, k = -4;
 
-	printf("введите х:\n");
+	printf("ГўГўГҐГ¤ГЁГІГҐ Гµ:\n");
 	scanf("%f", &x);
 
 	a = log((-1) * k * x);
 	b = exp(2 * x) + (a * x);
 	y = x * (a * a * a) + (b * b);
-	printf("Ответ: y=%.1f\n", y);
+	printf("ГЋГІГўГҐГІ: y=%.1f\n", y);
 	printf(" \n");
 	printf("(a=%f   b=%f)\n", a, b);
 }
